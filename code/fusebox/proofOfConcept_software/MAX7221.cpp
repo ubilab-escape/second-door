098075f37a72f19d60a65e59e3b0e024d8da91cc
JayWiz <Johannes.Aslan98@gmail.com>
2019-11-27 18:18:01 +0100

    Added Header and cpp File for custom MAX7221 library, removed TM1637. Changed connection of Potis, implemented simple logic for Potis.

---
+++ code/fusebox/proofOfConcept_software/MAX7221.cpp
@@ -0,0 +1,38 @@
+#include "MAX7221.h"
+#include <SPI.h>
+
+
+MAX7221::MAX7221(uint8_t csPin, uint8_t numberOfDigits = 1) {
+    cs_pin_ = csPin;
+    number_of_digits_ = numberOfDigits;
+}
+
+void MAX7221::transferData(uint8_t address, uint8_t value) {
+
+    // TODO: Implement daisy chaining
+
+    digitalWrite(cs_pin_, LOW);
+    SPI.transfer(address);
+    SPI.transfer(value);
+    digitalWrite(cs_pin_, HIGH);
+}
+
+void MAX7221::initMAX() {
+    pinMode(cs_pin_, OUTPUT);
+    
+    SPI.setBitOrder(MSBFIRST);
+    SPI.begin();
+
+    // enable decode mode
+    transferData(0x09, 0x0F); // Code B decode for digits 3-0 No Decode for digits 7 - 4
+
+    // set intensity
+    transferData(0x0A, 0x00); // Duty Cycle 1/16 (min on) 
+
+    // set scan-limit
+    transferData(0x0B, 0x03); // Display Digits 0 1 2 3
+
+    // enter normal operation mode
+    transferData(0x0C, 0x01); // Normal Operation   
+    Serial.println("MAX init done");
+}
--- code/fusebox/proofOfConcept_software/proofOfConcept_software.ino
+++ code/fusebox/proofOfConcept_software/proofOfConcept_software.ino
@@ -1,5 +1,5 @@
-#include <TM1637Display.h>
-#include <SPI.h>
+
+#include "MAX7221.h"
 
 #if CONFIG_FREERTOS_UNICORE
 #define ARDUINO_RUNNING_CORE 0
@@ -7,25 +7,12 @@
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
-
-
-
-
-#define SEG_CLK 27
-#define SEG_DIO 26
-
-
-
-
-#define MA7221_CS 14
-
-
+#define MAX7221_CS 14
 
 #define POTI_0 15
-#define POTI_1 2
-#define POTI_2 0
-#define POTI_3 4
-
+#define POTI_1 4
+#define POTI_2 27
+#define POTI_3 26
 
 #define PIEZO_0 13
 
@@ -41,7 +28,8 @@
 #define REWIRE_0_4 35
 #define REWIRE_0_5 34
 
-TM1637Display display(SEG_CLK, SEG_DIO);
+
+MAX7221 max1 = MAX7221(MAX7221_CS, 4);
 
 
 uint16_t evaluatePotentiometer(uint8_t p_poti);
@@ -52,22 +40,6 @@ void TaskWiringReadout(void *pvParameters);
 void TaskPiezoButtonReadout(void *pvParameters);
 void TaskControlLedMatrix(void *pvParameters);
 
-void maxTransfer(uint8_t address, uint8_t value) {
-
-  // Ensure LOAD/CS is LOW
-  digitalWrite(MA7221_CS, LOW);
-
-  // Send the register address
-  SPI.transfer(address);
-
-  // Send the value
-  SPI.transfer(value);
-
-  // Tell chip to load in data
-  digitalWrite(MA7221_CS, HIGH);
-}
-
-
 void setup() {
 
   // TODO: Clean Setup
@@ -76,55 +48,27 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Setup started");
   
-  display.setBrightness(0x0a);
-
+  
   pinMode(BUTTON_0, INPUT);
   pinMode(BUTTON_1, INPUT);
 
   ledcSetup(LEDC_CHANNEL1, 2000, LEDC_RESOLUTION);
   ledcAttachPin(PIEZO_0, LEDC_CHANNEL1);
 
+  max1.initMAX();
+  max1.transferData(0x01, 0x08);
+  delay(500);
+  max1.transferData(0x01, 0x02);
+  max1.transferData(0x02, 0x00);
+  delay(500);
 
-  // MAX7221 Configuration
-  pinMode(MA7221_CS, OUTPUT);
-  SPI.setBitOrder(MSBFIRST);
-  SPI.begin();
+  Serial.println("Setup finished");
 
-  
-  // TODO: https://gist.github.com/nrdobie/8193350
-
-  // disable decode mode
-  maxTransfer(0x09, 0x00);
-
-  // set intensity, medium
-  maxTransfer(0x0A, 0x01);
-
-  // enter normal operation mode
-  maxTransfer(0x0C, 0x01);
-
-  // enable segDP, segA, ... , segG
-  maxTransfer(0x01, 0xFF);
-
-  for (uint8_t i = 0x01; i < 0x0F; i++) {
-    maxTransfer(0x0A, i);
-    delay(250);
-  }
-
-  for (uint8_t i = 0x0F; i > 0x00; i--) {
-    maxTransfer(0x0A, i);
-    delay(250);
-  }
-  
-
-
-
-  
+ 
   xTaskCreatePinnedToCore(TaskPotentiometerReadout, "TaskPotentiometerReadout", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
   xTaskCreatePinnedToCore(TaskWiringReadout, "TaskWiringReadout", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
   xTaskCreatePinnedToCore(TaskPiezoButtonReadout, "TaskPiezoButtonReadout", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
-  // xTaskCreatePinnedToCore(TaskControlLedMatrix, "TaskControlLedMatrix", 1024, NULL, 2, NULL, ARDUINO_RUNNING_CORE);
 
-  Serial.println("Setup finished");
 }
 
 void loop() {
@@ -139,9 +83,21 @@ void TaskPotentiometerReadout(void *pvParameters) {
     uint8_t potis[] = {POTI_0, POTI_1, POTI_2, POTI_3};
     for (int i = 0; i <= 3; i++) {
       pValues[i] = evaluatePotentiometer(potis[i], 0, 4095, 0, 9);
+      Serial.print("Poti ");
+      Serial.print(i);
+      Serial.print(": ");
+      Serial.println(pValues[i]);
     }
-    setDisplay(pValues);
-    vTaskDelay(100);
+    max1.transferData(0x01, pValues[2]);
+    max1.transferData(0x02, pValues[3]);
+
+    if (pValues[0] == 4 && pValues[1] == 3 && pValues[2] == 2 && pValues[3] == 1) {
+      Serial.println("Potis Solved!"); 
+    } else {
+      Serial.println("Potis Not solved!");
+    }
+    
+    vTaskDelay(1000);
   }
 }
 
@@ -155,10 +111,12 @@ void TaskWiringReadout(void *pvParameters) {
       rewiringValues0[i] = analogRead(rewiringPins0[i]);
       rewiringValues0[i] = map(rewiringValues0[i], 0, 4095, 0, 33);
       // Serial.print("Pin " + String(i) + " : ");
+      Serial.print("Rewiring 0_");
+      Serial.print(i);
+      Serial.print(": ");
       Serial.println(rewiringValues0[i]);
     }
-    Serial.println("Test");
-    vTaskDelay(500);
+    vTaskDelay(2000);
   }
 }
 
@@ -189,22 +147,6 @@ void TaskPiezoButtonReadout(void *pvParameters) {
       ledcWriteTone(LEDC_CHANNEL1, 0);
     }
   }
-}
-
-// void TaskControlLedMatrix(void *pvParameters) {
-//   (void) pvParameters;
-//   for(;;) {
-//     maxTransfer()
-    
-//   }
-// }
-
-void setDisplay(uint16_t* p_displayData) {
-  static uint8_t data[4];
-  for (int i = 0; i < 4; i++) {
-    data[i] = display.encodeDigit(p_displayData[i]);
-  }
-  display.setSegments(data);
 }
 
 uint16_t evaluatePotentiometer(uint8_t p_poti, uint16_t in_min, uint16_t in_max, uint16_t out_min, uint16_t out_max) {
