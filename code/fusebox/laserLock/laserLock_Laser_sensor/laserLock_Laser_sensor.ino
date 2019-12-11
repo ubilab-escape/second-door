48cee4dfc1a46ecc96b3acd258f0ea08a1cd4570
TKoe92 <t.koetzner@gmx.de>
2019-12-11 19:05:26 +0100

    add bink if puzzle lock is broken, fixd bug: ring did not become green if more than one minute ther was no detection

--- code/fusebox/laserLock/laserLock_Laser_sensor/laserLock_Laser_sensor.ino
+++ code/fusebox/laserLock/laserLock_Laser_sensor/laserLock_Laser_sensor.ino
@@ -2,6 +2,8 @@
 
 #include <Adafruit_NeoPixel.h>
 
+#define MAX_SEQUENZES 32
+
 #define LASER_PIN          21 
 #define SEQUENDE_SIZE       5
 
@@ -23,6 +25,8 @@ uint8_t numberOfSequences = 0;
 
 int old_time_in_ms = millis();
 
+bool puzzle_solved = false;
+
 //Functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -37,6 +41,23 @@ uint8_t  analyse_sequence(uint8_t sequence[6], uint8_t target){
   return target_number + 1;
 }
 
+void blink_ring(uint8_t blinking_number, uint8_t frequency){
+  uint16_t period = 1 * 1000 / frequency;
+  for (uint8_t k=0; k < blinking_number; k++){
+    for(int i=0;i<NUM_PIXEL;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,0,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    }
+    delay(period/2);
+    for(int i=0;i<NUM_PIXEL;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,255,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    }
+    delay(period/2);
+    }
+  }
 
 //Setup
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,7 +68,7 @@ void setup() {
 
    // set led ring to red
   pixels.begin();
-  pixels.setBrightness(50); //die Helligkeit setzen 0 dunke -> 255 ganz hell
+  pixels.setBrightness(255); //die Helligkeit setzen 0 dunke -> 255 ganz hell
   pixels.show(); // Alle NeoPixel sind im Status "aus".
   
   for(int i=0;i<NUM_PIXEL;i++){
@@ -63,44 +84,53 @@ void setup() {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  Serial.println("Meas");
-
-  static uint8_t byte_count  = 0;
+  
+  static uint8_t byte_count = 0;
+  
   sequence[byte_count] = digitalRead(detectorPin);
-
+  
   if (byte_count == SEQUENDE_SIZE){
     byte_count = 0;
-    uint8_t zeros = analyse_sequence(sequence, 0);
+    uint8_t zeros = analyse_sequence(sequence, 0); 
     uint8_t ones = analyse_sequence(sequence, 1);
     
-    if (zeros == ones){
+    //chekc if sequenz is correct
+    if (zeros == ones && !puzzle_solved){
       Serial.print("Sequence dedected ");
       Serial.print(zeros);Serial.print(" ");Serial.println(ones);
       numberOfSequences++;
-      old_time_in_ms = millis(); 
-
-      if (numberOfSequences % 2 == 0){
-            uint8_t RGB_led = (uint8_t)numberOfSequences/2;
-            pixels.setPixelColor(RGB_led, pixels.Color(0,255,0)); // Moderately bright green color.
-            pixels.show(); // This sends the updated pixel color to the hardware.
-        }
-      
-      if (numberOfSequences == 32){
-        Serial.println("Fuse Box open");
-        numberOfSequences = 0;
+      old_time_in_ms = millis();
       }
+     }
+     
+    // check if new additional LED shoulb set to green
+    if ((numberOfSequences % 2 == 0) && (numberOfSequences > 0)){
+          uint8_t RGB_led = (uint8_t)numberOfSequences/2;
+          pixels.setPixelColor(RGB_led, pixels.Color(0,255,0)); // Moderately bright green color.
+          pixels.show(); // This sends the updated pixel color to the hardware.
+      }
+      
+    //check if puzzle is solved
+    if (numberOfSequences > MAX_SEQUENZES){
+      puzzle_solved = true;
+      Serial.println("Fuse Box open");
+      numberOfSequences = 0;
+      blink_ring(5, 2);
     }
+    
     
     int time_in_ms = millis() - old_time_in_ms;
 
     if (time_in_ms >= 500){
       if (numberOfSequences > 0){
-        numberOfSequences--;
+        if(numberOfSequences % 2 == 0){
+          uint8_t RGB_led = (uint8_t)numberOfSequences/2;
+          pixels.setPixelColor(RGB_led, pixels.Color(255,0,0)); // set led to red
+          pixels.show(); // This sends the updated pixel color to the hardware.     
+          }         
+        numberOfSequences--;   // count down sequence if max time was reached  
     }
-    
+    old_time_in_ms = millis();    
   }
-
-  
-  byte_count++;
-}
+byte_count++;
 }
