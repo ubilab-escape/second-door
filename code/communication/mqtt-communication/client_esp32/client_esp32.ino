b1f575daf7efd28696876712dcb5dded9931029a
TKoe92 <t.koetzner@gmx.de>
2019-12-18 14:01:32 +0100

    add communication folder

---
+++ code/communication/mqtt-communication/client_esp32/client_esp32.ino
@@ -0,0 +1,14 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
