af12f66dbc63b2612461cf72a8aa8ec5dcc66bef
TKoe92 <t.koetzner@gmx.de>
2020-01-21 23:38:07 +0100

    set duty cycle of laser pin to zero in off state, fixed bug in init of rgb

--- code/laser/src/main.cpp
+++ code/laser/src/main.cpp
@@ -9,6 +9,8 @@
 // const char* mqtt_topic = "7/laser";
 const char* mqtt_server = "10.0.0.2";
 const char* mqtt_name = "LaserClient";
+const char* mqtt_topic_laser = "7/laser";
+
 
 #define LASER_PIN 21
 #define SEQUENDE_SIZE 5
@@ -28,32 +30,27 @@ MqttBase* mqtt_com;
 
 void callback(const char* method1, const char* state, int daten) {
   if (strcmp(method1, "TRIGGER") == 0) {
-    if (strcmp(state, "ON") == 0) {
-      // configure LED PWM functionalitites
-      ledcSetup(ledChannel, freq, resolution);
+    if (strcmp(state, "on") == 0) {
 
-      // attach the channel to the GPIO2 to be controlled
-      ledcAttachPin(LASER_PIN, ledChannel);
       ledcWrite(ledChannel, 512);
-
-      pixels.begin();
-      pixels.setBrightness(255);  // die Helligkeit setzen 0 dunke -> 255 ganz hell
-      pixels.show();              // Alle NeoPixel sind im Status "aus".
 
       for (int i = 0; i < NUM_PIXEL; i++) {
         // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
         pixels.setPixelColor(i, pixels.Color(0, 255, 0));  // Moderately bright red color.
         pixels.show();  // This sends the updated pixel color to the hardware.
       }
+      mqtt_com -> publish(mqtt_topic_laser,"STATUS", "active",0);
     }
-    if (strcmp(state, "OFF") == 0) {
-      pinMode(LASER_PIN, OUTPUT);
+    if (strcmp(state, "off") == 0) {
+      ledcWrite(ledChannel, 0);
 
       for (int i = 0; i < NUM_PIXEL; i++) {
         // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
         pixels.setPixelColor(i, pixels.Color(0, 0, 0));  // Moderately bright red color.
         pixels.show();  // This sends the updated pixel color to the hardware.
       }
+
+      mqtt_com -> publish(mqtt_topic_laser,"STATUS", "inactive",0);
     }
   }
 }
@@ -73,7 +70,19 @@ void setup() {
 
   mqtt_com = new MqttBase(mqtt_server, mqtt_name, 1883);
   mqtt_com->init(ssid, password, mqtt_topics, logic_callbacks);
-  // init_com(ssid, password, mqtt_server, callback);
+
+
+  // configure LED PWM functionalitites
+  ledcSetup(ledChannel, freq, resolution);
+  // attach the channel to the GPIO2 to be controlled
+  ledcAttachPin(LASER_PIN, ledChannel);
+  
+
+  //setup RGB
+  pixels.begin();
+  pixels.setBrightness(255); 
+  //communicate status to operater at start up
+  mqtt_com -> publish(mqtt_topic_laser,"STATUS", "inactive",0);
 }
 
 // Loop
